c4e382680e8de7f897d319f43fb6c7b97e42b3f3
09453590382 <paragesawrich211@gmail.com>
2026-08-18 21:52:34 +0800

    Configure Aiven database SSL

--- config/database.php
+++ config/database.php
@@ -9,12 +9,6 @@ return [
     |--------------------------------------------------------------------------
     | Default Database Connection Name
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
     */
 
     'default' => env('DB_CONNECTION', 'sqlite'),
@@ -23,11 +17,6 @@ return [
     |--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported by Laravel. You're free to add / remove connections.
-    |
     */
 
     'connections' => [
@@ -35,7 +24,10 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env(
+                'DB_DATABASE',
+                database_path('database.sqlite')
+            ),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
@@ -44,24 +36,55 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
+        /*
+        |--------------------------------------------------------------------------
+        | MySQL / Aiven Database
+        |--------------------------------------------------------------------------
+        */
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
+
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
+
             'unix_socket' => env('DB_SOCKET', ''),
+
             'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'collation' => env(
+                'DB_COLLATION',
+                'utf8mb4_unicode_ci'
+            ),
+
             'prefix' => '',
             'prefix_indexes' => true,
+
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+
+            /*
+            |--------------------------------------------------------------------------
+            | Aiven SSL Certificate
+            |--------------------------------------------------------------------------
+            |
+            | The certificate must be located at:
+            |
+            | certs/aiven-ca.pem
+            |
+            */
+
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    (PHP_VERSION_ID >= 80500
+                        ? Mysql::ATTR_SSL_CA
+                        : PDO::MYSQL_ATTR_SSL_CA
+                    ) => base_path('certs/aiven-ca.pem'),
+                ])
+                : [],
         ],
 
         'mariadb' => [
@@ -74,14 +97,23 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'collation' => env(
+                'DB_COLLATION',
+                'utf8mb4_unicode_ci'
+            ),
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+
+            'options' => extension_loaded('pdo_mysql')
+                ? array_filter([
+                    (PHP_VERSION_ID >= 80500
+                        ? Mysql::ATTR_SSL_CA
+                        : PDO::MYSQL_ATTR_SSL_CA
+                    ) => base_path('certs/aiven-ca.pem'),
+                ])
+                : [],
         ],
 
         'pgsql' => [
@@ -110,8 +142,6 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
@@ -120,11 +150,6 @@ return [
     |--------------------------------------------------------------------------
     | Migration Repository Table
     |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
     */
 
     'migrations' => [
@@ -136,11 +161,6 @@ return [
     |--------------------------------------------------------------------------
     | Redis Databases
     |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
     */
 
     'redis' => [
@@ -149,7 +169,14 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+
+            'prefix' => env(
+                'REDIS_PREFIX',
+                Str::slug(
+                    (string) env('APP_NAME', 'laravel')
+                ) . '-database-'
+            ),
+
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
@@ -161,7 +188,10 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
             'max_retries' => env('REDIS_MAX_RETRIES', 3),
-            'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
+            'backoff_algorithm' => env(
+                'REDIS_BACKOFF_ALGORITHM',
+                'decorrelated_jitter'
+            ),
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
         ],
@@ -174,7 +204,10 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
             'max_retries' => env('REDIS_MAX_RETRIES', 3),
-            'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
+            'backoff_algorithm' => env(
+                'REDIS_BACKOFF_ALGORITHM',
+                'decorrelated_jitter'
+            ),
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
         ],
