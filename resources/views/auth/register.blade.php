928df007d103239e7d50726e4f678ae20bb077c4
Kilo <kilo@example.com>
2026-09-05 12:41:32 +0800

    Use templib.png as login and register page background
    - login.blade.php: bg-image changed from bg.png to templib.png
    - register.blade.php: bg-image changed from bg.png to templib.png for consistency

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <style>
         :root {
-            --bg-image: url('{{ asset('images/bg.png') }}');
+            --bg-image: url('{{ asset('images/templib.png') }}');
             --header-image: url('{{ asset('images/templib.png') }}');
         }
         
