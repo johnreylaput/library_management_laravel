f2700142a74db82c3625e8770c87f88d53f2f6b2
Kilo <kilo@example.com>
2026-09-14 14:24:26 +0800

    Add rate limiting to login and register routes (5 attempts per minute)

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ use App\Http\Controllers\Member\ReservationController as MemberReservationContro
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:5,1');
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
