2bda96b89ac8becab1a606878a86820cf4b925f1
Kilo <kilo@example.com>
2026-09-18 13:03:20 +0800

    Seeders: use only new thesis data structure

--- database/seeders/IntaoThesisSeeder.php
+++ database/seeders/IntaoThesisSeeder.php
@@ -19,31 +19,25 @@ class IntaoThesisSeeder extends Seeder
 
         $thesisData = [
             'title' => 'Self-Efficacy and Work Satisfaction of Non-Teaching Personnel',
-            'authors' => 'Mary Jane A. Intao',
-            'thesis_type' => 'Masters',
+            'author' => 'Mary Jane A. Intao',
+            'research' => 'Masteral Thesis',
             'institution' => 'Guimaras State University',
-            'year' => 2025,
+            'date_published' => '2025-01-15',
             'pages' => '1-502',
             'category' => 'Psychology',
-            'author' => 'Mary Jane A. Intao',
             'publisher' => 'Cambridge University Press',
-                    'link' => 'https://scholar.google.com/scholar?q=Self-Efficacy+and+Work+Satisfaction+of+Non-Teaching+Personnel+Mary+Jane+A.+Intao',
-            'abstract' => 'This study aimed to determine the level of self-efficacy and the work satisfaction of Non-Teaching personnel of St. Vincent College all located in Panay, Philippines for the academic year 2024-2025.',
+            'link' => 'https://scholar.google.com/scholar?q=Self-Efficacy+and+Work+Satisfaction+of+Non-Teaching+Personnel+Mary+Jane+A.+Intao',
+            'summary' => 'This study aimed to determine the level of self-efficacy and the work satisfaction of Non-Teaching personnel of St. Vincent College all located in Panay, Philippines for the academic year 2024-2025.',
             'description' => 'The study utilized a descriptive research design to investigate the level of self-efficacy and work satisfaction among 42 non-teaching personnel from three campuses of St. Vincent College in Panay Island, Philippines.',
             'database_collection' => 'Guimaras State University Repository',
             'availability' => 'Available',
-            'subjects' => 'Self-Efficacy, Work Satisfaction, Non-Teaching Personnel',
+            'subjects_keywords' => 'Self-Efficacy, Work Satisfaction, Non-Teaching Personnel',
             'status' => 'Available',
         ];
 
         $category = $categories->get($thesisData['category']);
         $author = $authors->get($thesisData['author']);
         $publisher = $publishers->get($thesisData['publisher']);
-
-        $researchMap = [
-            'PhD' => 'Doctoral Thesis',
-            'Masters' => 'Masteral Thesis',
-        ];
 
         if ($category && $author && $publisher) {
             Thesis::updateOrCreate(
@@ -53,18 +47,18 @@ class IntaoThesisSeeder extends Seeder
                 ],
                 [
                     'author' => $thesisData['author'],
-                    'research' => $researchMap[$thesisData['thesis_type']] ?? $thesisData['thesis_type'],
-                    'date_published' => '2025-01-15',
+                    'research' => $thesisData['research'],
+                    'date_published' => $thesisData['date_published'],
                     'pages' => $thesisData['pages'],
                     'category_id' => $category->id,
                     'author_id' => $author->id,
                     'publisher_id' => $publisher->id,
                     'link' => $thesisData['link'],
-                    'summary' => $thesisData['abstract'],
+                    'summary' => $thesisData['summary'],
                     'description' => $thesisData['description'],
                     'database_collection' => $thesisData['database_collection'],
                     'availability' => $thesisData['availability'],
-                    'subjects_keywords' => $thesisData['subjects'],
+                    'subjects_keywords' => $thesisData['subjects_keywords'],
                     'status' => $thesisData['status'],
                 ]
             );
